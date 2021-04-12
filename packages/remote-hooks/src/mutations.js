95e3292d7335e0ee813ce357043511eee8f48034
prifina-admin <tero@prifina.com>
2021-04-12 09:39:09 +0300

    remote-hooks

--- packages/remote-hooks/src/mutations.js
+++ packages/remote-hooks/src/mutations.js
@@ -3,10 +3,10 @@ export const getInfo = () => {
 };
 
 export const createMessage = (stage, appID, uuid, variables) => {
-  console.log("CREATE TEST ", stage);
-  console.log("CREATE TEST ", appID);
-  console.log("CREATE TEST ", uuid);
-  console.log("CREATE TEST ", variables);
+  console.log("CREATE MSG ", stage);
+  console.log("CREATE MSG ", appID);
+  console.log("CREATE MSG ", uuid);
+  console.log("CREATE MSG ", variables);
   if (stage === "dev") {
     return Promise.resolve({
       data: {
