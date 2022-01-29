2dfb6b6c655a34872dd257b9c5e3d1bc7e203a60
prifina-admin <tero@prifina.com>
2022-01-29 07:21:49 +0200

    data modules

--- packages/testData/stories/xx.js
+++ packages/testData/stories/xx.js
@@ -1,8 +1,19 @@
-const {ActivitySummary} =require("@prifina/oura-data/mockups");
-const {ActivitySummary:ActivitySummary2} =require("@prifina/oura-data/mockups/ActivitySummary");
-const OURA =require("@prifina/oura-data");
+const { ActivitySummary } = require("@prifina/oura-data/mockups");
+const {
+  ActivitySummary: ActivitySummary2,
+} = require("@prifina/oura-data/mockups/ActivitySummary");
+const OURA = require("@prifina/oura-data");
 
 console.log(ActivitySummary);
 
 console.log(ActivitySummary2);
 console.log(OURA);
+
+const { ActivitiesData } = require("@prifina/fitbit-data/mockups");
+console.log(ActivitiesData);
+
+const { DailiesData } = require("@prifina/garmin-data/mockups");
+console.log(DailiesData);
+
+const { Activity } = require("@prifina/google-data/mockups");
+console.log(Activity);
--- packages/testData/stories/xx.mjs
+++ packages/testData/stories/xx.mjs
@@ -1,7 +1,16 @@
-import {ActivitySummary} from "@prifina/oura-data/mockups";
+import { ActivitySummary } from "@prifina/oura-data/mockups";
 
 import OURA from "@prifina/oura-data";
 
-console.log(ActivitySummary)
+console.log(ActivitySummary);
 
-console.log(OURA)
+console.log(OURA);
+
+import { ActivitiesData } from "@prifina/fitbit-data/mockups";
+console.log(ActivitiesData);
+
+import { DailiesData } from "@prifina/garmin-data/mockups";
+console.log(DailiesData);
+
+import { Activity } from "@prifina/google-data/mockups";
+console.log(Activity);
